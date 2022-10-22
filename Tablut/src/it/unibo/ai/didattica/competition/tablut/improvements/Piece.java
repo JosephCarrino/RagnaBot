ab4080ae28ff6aa406fd5e19f62a6348b566d49a
JosephCarrino <josephcarrinospam@gmail.com>
2022-10-22 18:49:07 +0200

    Piece.java: added comments

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/improvements/Piece.java
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/improvements/Piece.java
@@ -4,8 +4,14 @@ package it.unibo.ai.didattica.competition.tablut.improvements;
 import it.unibo.ai.didattica.competition.tablut.domain.*;
 import it.unibo.ai.didattica.competition.tablut.domain.State.Pawn;
 
+/**
+ * Improvement of 'pawn' class, in order to save position and make harder operations
+ * 
+ * @author G. Carrino, M. Vannucchi
+ */
 public class Piece {
     
+    // Class for coordinates of a piece
     public class Position {
         public int x, y;
 
@@ -14,6 +20,10 @@ public class Piece {
             this.y = y;
         }
 
+        /***
+         * 
+         * @return An array of all reachable Positions starting from the current
+         */
         public Position[] getPossibleMoves() {
             Position[] possibleTo = new Position[16];
             int stateX = this.x;
@@ -36,17 +46,25 @@ public class Piece {
         }
     }
 
+        // Current piece position
         Position position = new Position(0, 0);
+
+        // The piece type, can be "W", "B" or "K"
         Pawn kind = Pawn.fromString("0");
+
+        // The current game state
         State state;
 
         public Piece(State s, Pawn kind, int xPos, int yPos) {
             this.state = s;
+
+            // Piece should not be empty pawn or throne
             if ((kind.toString() != "0") && (kind.toString() != "T"))
                 this.kind = kind;
             else
                 throw new IllegalStateException("Piece must be checker or king.");
-            this.position = new Position(xPos, yPos);
+            
+                this.position = new Position(xPos, yPos);
         }
 
         public String toString(){
